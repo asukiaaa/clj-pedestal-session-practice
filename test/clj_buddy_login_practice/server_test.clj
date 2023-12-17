28bde3e71dd2f26c4a779e31be5f5260d1e24d67
Asuki Kono <asukiaaa@gmail.com>
2023-12-17 12:29:34 +0900

    Create with using clj-new

---
+++ test/clj_buddy_login_practice/server_test.clj
@@ -0,0 +1,7 @@
+(ns clj-buddy-login-practice.server-test
+  (:require [clojure.test :refer :all]
+            [clj-buddy-login-practice.server :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
